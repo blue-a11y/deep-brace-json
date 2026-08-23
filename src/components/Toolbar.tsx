0c07fb1b69a006145937ac072101ca8f01865796
zhangxuan.0924 <zx_0828@126.com>
2026-08-23 22:14:31 +0800

    Separator 移入按钮内部(官方用法,修正上次静默失败的替换)

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -71,8 +71,8 @@ export function Toolbar() {
             <Braces size={14} />
             <span className="hidden sm:inline">格式化</span>
           </Button>
-          <ButtonGroup.Separator />
           <Button onPress={minify}>
+            <ButtonGroup.Separator />
             <Minimize2 size={14} />
             <span className="hidden sm:inline">压缩</span>
           </Button>
